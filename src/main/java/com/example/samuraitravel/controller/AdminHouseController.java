0fcfc7c568eab00e65922c684d1e899bf08a9100
poodle201 <mhf_shou@yahoo.co.jp>
2023-10-03 12:37:59 +0200

    「10章 管理者用の民宿詳細ページを作成しよう」まで完了

--- src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
+++ src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
@@ -1,11 +1,15 @@
 package com.example.samuraitravel.controller;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.samuraitravel.entity.House;
 import com.example.samuraitravel.repository.HouseRepository;
@@ -20,11 +24,29 @@ public class AdminHouseController {
     }	
     
     @GetMapping
-    public String index(Model model) {
-        List<House> houses = houseRepository.findAll();       
+    public String index(Model model, 
+    					@PageableDefault
+    					(page = 0, size = 10, sort = "id", direction = Direction.ASC)
+    					Pageable pageable,
+    					@RequestParam(name = "keyword", required = false)
+    					String keyword) {
+        Page<House> housePage;
+        if(keyword != null && !keyword.isEmpty()) {
+        	housePage = houseRepository.findByNameLike("%" + keyword + "%", pageable);
+        }else {
+        	housePage = houseRepository.findAll(pageable);
+        }
         
-        model.addAttribute("houses", houses);             
+        model.addAttribute("housePage", housePage);
+        model.addAttribute("keyword" ,keyword);
         
         return "admin/houses/index";
-    }  
+    }
+    
+    @GetMapping("/{id}")
+    public String show(@PathVariable(name="id") Integer id,Model model) {
+    	House house=houseRepository.getReferenceById(id);
+    	model.addAttribute("house", house);
+    	return "admin/houses/show";
+    }
 }
